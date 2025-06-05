5a4876b2ef5a8c597f2a3bb95de551189ad9cd95
MONIKA N <monikanagalingamn@gmail.com>
2025-06-05 11:45:17 +0530

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
 function fetchAirQuality() {
-    // Simulate API fetch (replace with real API call)
-    const randomPPM = (Math.random() * 300).toFixed(2); // Simulated value
+    
+    const randomPPM = (Math.random() * 300).toFixed(2); 
     document.getElementById('ppm-value').textContent = randomPPM;
   }
   
